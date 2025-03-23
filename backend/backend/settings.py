005a97e2ae71da83b95c69e77a04448870fa506f
mahdioshani <mahdioshani2003@gmail.com>
2025-03-23 22:10:04 +0330

    feat:Adding dockerfile and dockerCompose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(_=$cjl9=k%*zfgw*)*sny!-tn21##39fphwgtsr+o%gd&$+(k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
