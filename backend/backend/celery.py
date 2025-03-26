724a8d74e53db315b84d808e875513f60e329f9a
mahdioshani <mahdioshani2003@gmail.com>
2025-03-26 10:47:08 +0330

    fix:debugging celery

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -5,11 +5,11 @@ from kombu import Consumer, Exchange, Queue
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 import django
 django.setup()
 from analytics.models import Token
-app = Celery('proj')
+app = Celery('backend')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
