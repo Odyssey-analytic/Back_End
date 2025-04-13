c07fb89db0e25dd36e1b659a9df0aa4a8688a2ef
Mohammad <Mohammadshamekhi4@gmail.com>
2025-04-13 21:28:15 +0330

    change email password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -210,6 +210,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'oddysey.analytics@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+EMAIL_HOST_PASSWORD = 'cwle pfvr uhqc ezde'
 
 STATIC_ROOT= os.getcwd() + "/static"
